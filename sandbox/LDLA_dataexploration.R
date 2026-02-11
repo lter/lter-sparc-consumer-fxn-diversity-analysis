3353f189ac2ccd9e78dbf797c90d6a3e860528d1
leanderegg <leanderegg@gmail.com>
2026-02-11 15:29:08 -0800

    saved RDS

--- sandbox/LDLA_dataexploration.R
+++ sandbox/LDLA_dataexploration.R
@@ -225,7 +225,7 @@ onlytraits <- data.frame(all_traits.final %>% ungroup() %>% select(ends_with(".z
 rownames(onlytraits) <- all_traits.final$sp.proj
 #colnames(onlytraits) <- c("tr.age.zp", "tr.trophic.level.zp", "tr.reproductive.rate.zp", "tr.mass.adult.zp", "tr.active.time" )
 
-onlytraits.nat <- onlytraits %>% select(-tr.active.time,  -tr.reproductive.rate.zp, -tr.length.adult.zp)
+onlytraits.nat <- onlytraits %>% select(-tr.fecundity.zp, -tr.active.time,  -tr.reproductive.rate.zp, -tr.length.adult.zp)
 
 
 # # Build a dataframe gathering traits categories:
